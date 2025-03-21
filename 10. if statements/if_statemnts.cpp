7cccefc2cb9cc9b5bb54119f12234141d0601de0
clouqs <zetrox.gota@gmail.com>
2025-03-21 22:39:27 +0100

    Add age classification program using if statements

---
+++ 10. if statements/if_statemnts.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+    int age;
+    std::cout << "Enter your age: ";
+    if (!(std::cin >> age)){
+        std::cout << "Invalid input.";
+        return 1;
+    }
+    if (age >= 120){
+        std::cout << "You are a super adult.";
+    } else if (age >= 18){
+        std::cout << "You are an adult.";
+    } else {
+        std::cout << "You are a minor.";
+    }
+}
