648188d0e6de4c01bfce7896dd4e3c33a31cd065
clouqs <zetrox.gota@gmail.com>
2025-03-21 17:08:28 +0100

    Implement namespaces in C++ with example usage in main function

--- 3. namespace/namespace.cpp
+++ 3. namespace/namespace.cpp
@@ -1,1 +1,17 @@
-// namespace, speriamo che shinku fixa il cazzo di cheat
+#include <iostream>
+
+
+namespace first{
+    int x = 1;
+}
+
+namespace second{
+    int x = 2;
+}
+
+int main(){
+
+    int x = 0;
+    std::cout << first::x; //if i want to use x from first namespace i put "first::" before x
+    return 0;
+}
