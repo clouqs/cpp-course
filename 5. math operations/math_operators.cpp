a9b991058a181c7504ac7e2625469ad748c9fe7e
clouqs <zetrox.gota@gmail.com>
2025-03-21 19:02:44 +0100

    Update math_operators.cpp to demonstrate variable increment and output

--- 5. math operations/math_operators.cpp
+++ 5. math operations/math_operators.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
-#include <string>
 
 int main(){
-    std::string x = "ciao";
-    std::cout << x;
-    return 0;
+
+    int students = 26;
+    students++;
+    std::cout << students << '\n'; // outputs 27
+
+    //students--
+    //students += 5  adds 5 to students
+    //students / 3 divides students by 3
+    //students % 3 gives the remainder of students divided by 3
+    //students *= 3 multiplies students by 3
+    /*...*/
 }
