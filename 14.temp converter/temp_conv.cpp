73c8dcdb50edfe19e87f5f34ecae63f55ecad08b
clouqs <zetrox.gota@gmail.com>
2025-03-22 14:33:50 +0100

    Add temperature converter program with Celsius and Fahrenheit functions

--- 14.temp converter/temp_conv.cpp
+++ 14.temp converter/temp_conv.cpp
@@ -1,1 +1,36 @@
 #include <iostream>
+
+double cels2fahr(double cels) {
+    return cels * 9.0 / 5.0 + 32;
+}
+
+double fahr2cels(double fahr) {
+    return (fahr - 32) * 5.0 / 9.0;
+}
+
+int main(){
+
+    double fahr, cels;
+    int choice;
+    std::cout << "*****Temperature Converter*****\n";
+    std::cout << "1. Celsius to Fahrenheit\n";
+    std::cout << "2. Fahrenheit to Celsius\n";
+    std::cout << "Enter your choice: ";
+    std::cin >> choice;
+
+    switch(choice){
+        case 1:
+            std::cout << "Enter the temperature in Celsius: ";
+            std::cin >> cels;
+            std::cout << "Temperature in Fahrenheit: " << cels2fahr(cels);
+            break;
+        case 2:
+            std::cout << "Enter the temperature in Fahrenheit: ";
+            std::cin >> fahr;
+            std::cout << "Temperature in Celsius: " << fahr2cels(fahr);
+            break;
+        default:
+            std::cout << "Invalid choice.";
+            break;
+    }
+}
