3dfed154bf6c57be77a1633cbedd03d5ce473391
clouqs <zetrox.gota@gmail.com>
2025-03-22 22:47:35 +0100

    Add string methods demonstration with character access and length output

--- 15.string methods/str_methods.cpp
+++ 15.string methods/str_methods.cpp
@@ -1,1 +1,10 @@
+//string methods
 #include <iostream>
+
+int main(){
+    std::string str = "Hello World!";
+    int x = str.length();
+    char firstchar = str.at(0);
+    std::cout << "First character of string is: " << firstchar << std::endl;
+    std::cout << "\nLength of string is: " << x << std::endl;
+}
